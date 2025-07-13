3cc25067217e638e4ee13a97e03738d891ce9a96
HoweZae <jdtomanan@up.edu.ph>
2025-07-13 17:34:55 +0800

    chore: init tailwind config

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+export default {
+	theme: {
+		fontFamily: {},
+		extend: {
+			colors: {
+				transparent: "transparent",
+				currentColor: "currentColor",
+			}
+		},
+		screens: {
+			xs: "475px",
+			sm: "640px",
+			md: "768px",
+			lg: "1024px",
+			xl: "1280px",
+			xxl: "1536px",
+		},
+	},
+}
