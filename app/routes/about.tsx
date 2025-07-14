98dcedd721d8fc0e0b29a5fa7852abe34c17cad4
HoweZae <jdtomanan@up.edu.ph>
2025-07-14 10:36:55 +0800

    feat: rearrange subtitle; fix bug

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,11 +1,12 @@
 import type { Route } from "./+types/home";
+import { FiArrowUpRight } from "react-icons/fi";
 import { FaLinkedin, FaFacebookSquare, FaGithub } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 
 export function meta({}: Route.MetaArgs) {
 	return [
 		{ title: "About Me"},
-		{ name: "", content: "" }
+		{ name: "description", content: "..." }
 	];
 }
 
@@ -13,22 +14,19 @@ export default function About() {
 	return (
 		<>
 			<h1>About Me</h1>
-			<div className="flex items-baseline gap-2">
+			<div className="flex gap-2 items-center">
+				{socials.map(item => (
+					<a href={ item.link } target="_blank" className="about-icon">
+						{ item.icon }
+					</a>
+				))}
+				<span>&middot;</span>
 				<a href="https://josetmnn.vercel.app" target="_blank" className="hoverable-link">
 					portfolio
 				</a>
-				<span>&middot;</span>
-				{socials.map((
-						{icon, link, handle}) => (
-						<a href={link} target="_blank" className="about-icon">
-							{icon}
-						</a>
-				))}
 			</div>
 			<p>
-				This is my first project with React, in line with the principle of &ldquo;the best way to learn a framework, is to create something with it&rdquo;.
-			</p>
-			<p>
+				This is my first project with React; I made this in line with the principle of &ldquo;the best way to learn a framework, is to create something with it&rdquo;.
 				Hope you're impressed! ☺️
 			</p>
 		</>
@@ -37,23 +35,23 @@ export default function About() {
 
 const socials = [
 	{
+		key: "LI: jedtomanan",
 		icon: <FaLinkedin />,
 		link: "https://www.linkedin.com/in/jedtomanan/",
-		handle: "jedtomanan"
 	},
 	{
-		icon: <FaGithub />,
+		key: "Git: JoseTomanan",
 		link: "https://github.com/JoseTomanan",
-		handle: "JoseTomanan"
+		icon: <FaGithub />,
 	},
 	{
-		icon: <FaFacebookSquare />,
+		key: "FB: jedtomanan",
 		link: "https://www.facebook.com/jedtomanan/",
-		handle: "jedtomanan"
+		icon: <FaFacebookSquare />,
 	},
 	{
-		icon: <AiFillInstagram />,
+		key: "IG: jose.tmnn",
 		link: "https://www.instagram.com/jose.tmnn/",
-		handle: "jose.tmnn"
+		icon: <AiFillInstagram />,
 	},
 ];
