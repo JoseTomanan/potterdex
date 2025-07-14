15d96f9abdaf9f2ff8b273baa446aa0bce19ed29
HoweZae <jdtomanan@up.edu.ph>
2025-07-15 00:48:06 +0800

    feat: define simple search bar (incomplete)

--- app/components/Navbar/SearchBar.tsx
+++ app/components/Navbar/SearchBar.tsx
@@ -1,9 +1,18 @@
-
+import { FaSearch } from "react-icons/fa";
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
 
 export function SearchBar() {
-	return <div></div>;
+	return (
+		<form className="flex">
+			<Input placeholder="Search..." type="text" className="rounded rounded-r-none"/>
+			<Button
+				className="rounded rounded-l-none"
+				variant="default" size="icon">
+				<FaSearch />
+			</Button>
+		</form>
+
+		// TODO: add filters
+	);
 }
-
-const resources = [
-
-];
