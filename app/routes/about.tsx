ee2546cb2167fd00f8b1c80dda7547955d5d37e3
HoweZae <jdtomanan@up.edu.ph>
2025-07-14 14:37:45 +0800

    feat: vertical spacing

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -12,21 +12,24 @@ export function meta({}: Route.MetaArgs) {
 
 export default function About() {
 	return (
-		<main className="container">
-			<h1>About Me</h1>
-			<div className="flex gap-2 items-center">
-				{socials.map(({ href, icon }) => (
-					<a href={href} target="_blank" className="about-icon">
-						{icon}
+		<main className="flex flex-col mt-16 gap-6">
+			<div className="flex flex-col gap-1">
+				<h1>About Me</h1>
+				<span className="flex gap-2 items-center">
+					{socials.map(({ href, icon }, i) => (
+						<a href={href} target="_blank" className="about-icon" key={i}>
+							{icon}
+						</a>
+					))}
+					<span>&middot;</span>
+					<a href="https://josetmnn.vercel.app" target="_blank" className="hoverable-link">
+						portfolio
 					</a>
-				))}
-				<span>&middot;</span>
-				<a href="https://josetmnn.vercel.app" target="_blank" className="hoverable-link">
-					portfolio
-				</a>
+				</span>
 			</div>
 			<p>
 				This is my first project with React; I made this in line with the principle of &ldquo;the best way to learn a framework, is to create something with it&rdquo;.
+				<br/>
 				Hope you're impressed! ☺️
 			</p>
 		</main>
