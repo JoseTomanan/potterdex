5ae396efddb6751473d75f1a4b1759899375345a
HoweZae <jdtomanan@up.edu.ph>
2025-07-15 00:42:17 +0800

    feat: add content of logo (with placeholder)

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -1,3 +1,13 @@
+import { Link } from "react-router";
+import { GiOwl as InsideLogo } from "react-icons/gi";
+
 export function Logo() {
-	return <></>;
+	return (
+		<Link to="/" className="flex gap-2 items-center">
+			<span className="px-1">
+				<InsideLogo className="h-5 w-5 stroke-2"/>
+			</span>
+			<span className="text-xl font-medium">PotterDex</span>
+		</Link>
+	);
 }
