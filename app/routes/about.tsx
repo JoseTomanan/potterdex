edaf9cf091ac7bb7b1d3e2713cec9e51be8dce5a
HoweZae <jdtomanan@up.edu.ph>
2025-07-14 09:38:45 +0800

    feat: add semi-good placeholders

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,4 +1,6 @@
 import type { Route } from "./+types/home";
+import { FaLinkedin, FaFacebookSquare, FaGithub } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
 
 export function meta({}: Route.MetaArgs) {
 	return [
@@ -8,12 +10,50 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function About() {
-	return <>
-		<h1>About Me</h1>
-		<h6>
-			<a className="hoverable-link">
-				My full portfolio
-			</a>
-		</h6>
-	</>
+	return (
+		<>
+			<h1>About Me</h1>
+			<div className="flex items-baseline gap-2">
+				<a href="https://josetmnn.vercel.app" target="_blank" className="hoverable-link">
+					portfolio
+				</a>
+				<span>&middot;</span>
+				{socials.map((
+						{icon, link, handle}) => (
+						<a href={link} target="_blank" className="about-icon">
+							{icon}
+						</a>
+				))}
+			</div>
+			<p>
+				This is my first project with React, in line with the principle of &ldquo;the best way to learn a framework, is to create something with it&rdquo;.
+			</p>
+			<p>
+				Hope you're impressed! ☺️
+			</p>
+		</>
+	);
 }
+
+const socials = [
+	{
+		icon: <FaLinkedin />,
+		link: "https://www.linkedin.com/in/jedtomanan/",
+		handle: "jedtomanan"
+	},
+	{
+		icon: <FaGithub />,
+		link: "https://github.com/JoseTomanan",
+		handle: "JoseTomanan"
+	},
+	{
+		icon: <FaFacebookSquare />,
+		link: "https://www.facebook.com/jedtomanan/",
+		handle: "jedtomanan"
+	},
+	{
+		icon: <AiFillInstagram />,
+		link: "https://www.instagram.com/jose.tmnn/",
+		handle: "jose.tmnn"
+	},
+];
