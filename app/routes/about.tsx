8b6ad7355d266111ef1171161efa7999963463a9
HoweZae <jdtomanan@up.edu.ph>
2025-07-15 11:57:58 +0800

    feat: use (section) container for about section

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -13,30 +13,32 @@ export function meta({}: Route.MetaArgs) {
 export default function About() {
 	return (
 		<main className="flex flex-col mt-16 gap-6">
-			<div className="flex flex-col gap-1">
-				<h1>About Me</h1>
-				<span className="flex gap-2 items-center">
-					{socials.map(({ href, icon }, i) => (
-						<a href={href} target="_blank" className="about-icon" key={i}>
-							{icon}
+			<section className="container">
+				<div className="flex flex-col gap-1">
+					<h1>About Me</h1>
+					<span className="flex gap-2 items-center">
+						{socials.map(({ href, icon }, i) => (
+							<a href={href} target="_blank" className="about-icon" key={i}>
+								{icon}
+							</a>
+						))}
+						<span>&middot;</span>
+						<a href="https://josetmnn.vercel.app" target="_blank" className="hoverable-link">
+							portfolio
 						</a>
-					))}
-					<span>&middot;</span>
-					<a href="https://josetmnn.vercel.app" target="_blank" className="hoverable-link">
-						portfolio
-					</a>
-				</span>
-			</div>
-			<div>
-				<p>
-					This is my first project with React. I made this project in line with the principle of &ldquo;the best way to learn a framework, is to create something with it&rdquo;.
-					<br/>
-					I hope you're impressed!
-				</p>
-				<p>
-					The repository of this web app can be found <a href="https://github.com/JoseTomanan/potterdex" className="hoverable-link">here</a>.
-				</p>
-			</div>
+					</span>
+				</div>
+				<div>
+					<p>
+						This is my first project with React. I made this project in line with the principle of &ldquo;the best way to learn a framework, is to create something with it&rdquo;.
+						<br/>
+						I hope you're impressed!
+					</p>
+					<p>
+						The repository of this web app can be found <a href="https://github.com/JoseTomanan/potterdex" className="hoverable-link">here</a>.
+					</p>
+				</div>
+			</section>
 		</main>
 	);
 }
