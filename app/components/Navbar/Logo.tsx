f0d17394faf2179a72cb1755765e4dcb1d8fc5b6
HoweZae <jdtomanan@up.edu.ph>
2025-07-15 12:01:13 +0800

    feat: use body font for navbar

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -5,7 +5,7 @@ export function Logo() {
 	return (
 		<Link to="/" className="flex gap-2 items-center group">
 			<InsideLogo className="h-6 w-6 stroke-2"/>
-			<span className="text-xl font-medium group-hover:text-foreground group-hover:text-shadow-lg group-hover:text-shadow-muted">
+			<span className="text-xl font-heading font-medium group-hover:text-foreground group-hover:text-shadow-lg group-hover:text-shadow-muted">
 				PotterDex
 			</span>
 		</Link>
