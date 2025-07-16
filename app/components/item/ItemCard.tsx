40bc21a1c5a338085e1ea54e42c38a2fe7d64081
HoweZae <jdtomanan@up.edu.ph>
2025-07-16 13:19:41 +0800

    feat: add placeholder for attributes (partially working, unstyled)

--- app/components/item/ItemCard.tsx
+++ app/components/item/ItemCard.tsx
@@ -1,13 +1,19 @@
 
 type ItemCardProps = {
-	name: String;
-
+	name: string;
+	house: string;
+	nationality: string;
 }
 
 export function ItemCard( item: ItemCardProps ) {
 	return (
 		<div className="card">
-			<h2>{ item.name }</h2>
+			<h3>{ item.name }</h3>
+			<div className="flex flex-row gap-x-1">
+				<h5>{ item.house }</h5>
+				<span> &middot; </span>
+				<h5>{ item.nationality }</h5>
+			</div>
 		</div>
 	);
 }
