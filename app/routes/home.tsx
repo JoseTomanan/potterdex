897bdfc66791e8cf7ef13d3ae73b977a6b677572
HoweZae <jdtomanan@up.edu.ph>
2025-07-16 11:48:47 +0800

    feat: add initializers, statekeeping

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,17 +1,45 @@
+import {
+	useEffect,
+	useState,
+} from "react";
 import type { Route } from "./+types/home";
+import {
+	createCharacter as createItem,
+	type Character as Item,
+} from "~/lib/types/character";
 import { ItemGrid } from "~/components/item/ItemGrid";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "POKÉDEX | Search" },
-    { name: "description", content: "Pokedex with React" },
+    { title: "POTTERDEX | Database" },
+    { name: "description", content: "Pokedex for Harry Potter" },
   ];
 }
 
+
 export default function Home() {
+	const [items, setItems] = useState<Item[]>([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		// Simulated loading for now
+		setTimeout(() => {
+			setItems([
+				createItem({ id: '1', name: 'Eddie Alvarez', nationality: 'American'}),
+				createItem({ id: '2', name: 'Jairzinho Rozenstruik', nationality: 'Surinamese'}),
+				createItem({ id: '3', name: 'Khamzat Chimaev', nationality: 'Chechnyan'}),
+				createItem({ id: '4', name: 'Rafael Fiziev', nationality: 'Azerbaijani'}),
+				createItem({ id: '5', name: 'Dustin Poirier', nationality: 'American'}),
+			]);
+		}, 1500);
+	}, []);
+
   return (
 		<main>
-			<ItemGrid />
+			{/* TODO: PAGINATION */}
+
+			<ItemGrid items={items} isLoading={isLoading} />
 		</main>
 	);
 }
