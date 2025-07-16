5d1dbae75c7d3a6a85d05079b72dd6cd7d1867b0
HoweZae <jdtomanan@up.edu.ph>
2025-07-16 12:52:57 +0800

    feat: change color of skeletons

--- app/components/item/ItemSkeleton.tsx
+++ app/components/item/ItemSkeleton.tsx
@@ -2,11 +2,12 @@ import { Skeleton } from "~/components/ui/skeleton";
 
 export function ItemSkeleton() {
 	return (
-		<div className="flex flex-col gap-y-3">
-			<Skeleton className="h-[125px] w-[250px] rounded" />
+		<div className="card gap-y-3">
+			<Skeleton className="bg-muted h-[125px] w-[280px] rounded" />
 			<div className="space-y-2">
-				<Skeleton className="h-4 w-[250px]" />
-				<Skeleton className="h-4 w-[200px]" />
+				<Skeleton className="bg-muted h-4 w-[240px]" />
+				<Skeleton className="bg-muted h-4 w-[200px]" />
+				<Skeleton className="bg-muted h-4 w-[100px]" />
 			</div>
 		</div>
 	);
