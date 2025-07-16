e7d0947019d302e985e0a63cd3e947f487248ef4
HoweZae <jdtomanan@up.edu.ph>
2025-07-16 09:17:02 +0800

    chore: clarity in wording

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -29,8 +29,8 @@ export default function About() {
 					<p>
 						This is my first project with React; I made this in line with the principle of &ldquo;the best way to learn a framework, is to create something with it&rdquo;.
 						<br />
-						The repository of this web app can be found <a href="https://github.com/JoseTomanan/potterdex" className="hoverable-link">here</a>.
-						You can also visit my <a href="https://josetmnn.vercel.app" target="_blank" className="hoverable-link">portfolio</a>.
+						You can visit this app's <a href="https://github.com/JoseTomanan/potterdex" className="hoverable-link"> Git repository</a>.
+						You can also visit my <a href="https://josetmnn.vercel.app" target="_blank" className="hoverable-link">portfolio webpage</a>.
 					</p>
 				</div>
 
