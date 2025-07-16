760125dc32ac5edc2ed2d81709b8c1bc4f456163
HoweZae <jdtomanan@up.edu.ph>
2025-07-16 16:09:17 +0800

    feat: add portrait; move skeleton to inside card

--- app/components/item/ItemCard.tsx
+++ app/components/item/ItemCard.tsx
@@ -1,18 +1,45 @@
+import { GoPerson, GoPersonFill } from "react-icons/go";
+import { Skeleton } from "../ui/skeleton";
 
 type ItemCardProps = {
 	name: string;
 	house: string;
 	nationality: string;
+	gender: string;
+	image: string;
 }
 
 export function ItemCard( item: ItemCardProps ) {
 	return (
-		<div className="card">
-			<h3>{ item.name }</h3>
-			<div className="flex flex-row gap-x-1">
+		<div className="card gap-y-2">
+			<span className="bg-primary-foreground h-[125px] flex justify-center">
+				{item.image ? (
+					<img src={item.image}/>
+				) : (
+					<GoPersonFill className="h-[125px] w-[125px] fill-muted" />
+				)}
+			</span>
+			<div>
+				<h3>{ item.name }</h3>
 				<h5>{ item.house }</h5>
-				<span> &middot; </span>
-				<h5>{ item.nationality }</h5>
+				<span className="flex flex-row gap-x-1">
+					<h5>{ item.gender }</h5>
+					<span>&middot;</span>
+					<h5>{ item.nationality }</h5>
+				</span>
+			</div>
+		</div>
+	);
+}
+
+export function ItemSkeleton() {
+	return (
+		<div className="card gap-y-3">
+			<Skeleton className="bg-muted h-[125px] w-[280px] rounded" />
+			<div className="space-y-2">
+				<Skeleton className="bg-muted h-4 w-[240px]" />
+				<Skeleton className="bg-muted h-4 w-[200px]" />
+				<Skeleton className="bg-muted h-4 w-[100px]" />
 			</div>
 		</div>
 	);
