c8a18bb8db50cde7fc2d4d32dee8868f25e2302d
HoweZae <jdtomanan@up.edu.ph>
2025-07-16 13:14:35 +0800

    feat: add unworking pagination

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -7,7 +7,17 @@ import {
 	createCharacter as createItem,
 	type Character as Item,
 } from "~/lib/types/character";
+
 import { ItemGrid } from "~/components/item/ItemGrid";
+import {
+	Pagination,
+	PaginationContent,
+	PaginationEllipsis,
+	PaginationItem,
+	PaginationLink,
+	PaginationNext,
+	PaginationPrevious,
+} from "~/components/ui/pagination"
 
 
 export function meta({}: Route.MetaArgs) {
@@ -27,33 +37,52 @@ export default function Home() {
 		setTimeout(() => {
 			setIsLoading(false);
 			setItems([
-				createItem({ id: '1', name: 'Eddie Alvarez', nationality: 'American'}),
-				createItem({ id: '2', name: 'Jairzinho Rozenstruik', nationality: 'Surinamese'}),
-				createItem({ id: '3', name: 'Khamzat Chimaev', nationality: 'Chechnyan'}),
-				createItem({ id: '4', name: 'Rafael Fiziev', nationality: 'Azerbaijani'}),
-				createItem({ id: '5', name: 'Dustin Poirier', nationality: 'American'}),
-				createItem({ id: '6', name: 'Charles Oliveira', nationality: 'Brazilian' }),
-				createItem({ id: '7', name: 'Islam Makhachev', nationality: 'Russian' }),
-				createItem({ id: '8', name: 'Ilia Topuria', nationality: 'Georgian' }),
-				createItem({ id: '9', name: 'Leon Edwards', nationality: 'Jamaican-British' }),
-				createItem({ id: '10', name: 'Brandon Moreno', nationality: 'Mexican' }),
-				createItem({ id: '11', name: 'Julianna Peña', nationality: 'American' }),
-				createItem({ id: '12', name: 'Tai Tuivasa', nationality: 'Australian' }),
-				createItem({ id: '13', name: 'Ciryl Gane', nationality: 'French' }),
-				createItem({ id: '14', name: 'Valentina Shevchenko', nationality: 'Kyrgyzstani' }),
-				createItem({ id: '15', name: 'Paddy Pimblett', nationality: 'English' }),
-				createItem({ id: '16', name: 'Yair Rodríguez', nationality: 'Mexican' }),
-				createItem({ id: '17', name: 'Derrick Lewis', nationality: 'Jamaican' }),
-				createItem({ id: '18', name: 'Marvin Vettori', nationality: 'Italian' }),
-				createItem({ id: '19', name: 'Song Yadong', nationality: 'Chinese' }),
-				createItem({ id: '20', name: 'Alex Pereira', nationality: 'Brazilian' }),
+				createItem({ id: '1', name: 'Eddie Alvarez', nationality: 'Transylvanian', house: 'Ravenclaw' }),
+				createItem({ id: '2', name: 'Jairzinho Rozenstruik', nationality: 'Moroccan', house: 'Gryffindor' }),
+				createItem({ id: '3', name: 'Khamzat Chimaev', nationality: 'Bulgarian', house: 'Slytherin' }),
+				createItem({ id: '4', name: 'Rafael Fiziev', nationality: 'Scottish', house: 'Hufflepuff' }),
+				createItem({ id: '5', name: 'Dustin Poirier', nationality: 'Welsh', house: 'Slytherin' }),
+				createItem({ id: '6', name: 'Charles Oliveira', nationality: 'Greek', house: 'Gryffindor' }),
+				createItem({ id: '7', name: 'Islam Makhachev', nationality: 'Romanian', house: 'Ravenclaw' }),
+				createItem({ id: '8', name: 'Ilia Topuria', nationality: 'Irish', house: 'Gryffindor' }),
+				createItem({ id: '9', name: 'Leon Edwards', nationality: 'Austrian', house: 'Hufflepuff' }),
+				createItem({ id: '10', name: 'Brandon Moreno', nationality: 'Norwegian', house: 'Ravenclaw' }),
+				createItem({ id: '11', name: 'Julianna Peña', nationality: 'Hungarian', house: 'Gryffindor' }),
+				createItem({ id: '12', name: 'Tai Tuivasa', nationality: 'Slovenian', house: 'Slytherin' }),
+				createItem({ id: '13', name: 'Ciryl Gane', nationality: 'Danish', house: 'Ravenclaw' }),
+				createItem({ id: '14', name: 'Valentina Shevchenko', nationality: 'Estonian', house: 'Slytherin' }),
+				createItem({ id: '15', name: 'Paddy Pimblett', nationality: 'Moldovan', house: 'Hufflepuff' }),
+				createItem({ id: '16', name: 'Yair Rodríguez', nationality: 'Latvian', house: 'Gryffindor' }),
+				createItem({ id: '17', name: 'Jan Błachowicz', nationality: 'Croatian', house: 'Ravenclaw' }),
+				createItem({ id: '18', name: 'Marvin Vettori', nationality: 'Slovakian', house: 'Hufflepuff' }),
+				createItem({ id: '19', name: 'Zhang Weili', nationality: 'Czech', house: 'Gryffindor' }),
+				createItem({ id: '20', name: 'Alex Pereira', nationality: 'Ukrainian', house: 'Slytherin' }),
 			]);
 		}, 1500);
 	}, []);
 
   return (
 		<main>
-			{/* TODO: PAGINATION */}
+			<Pagination>
+				<PaginationContent>
+					<PaginationItem>
+						<PaginationPrevious href="#" />
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">1</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationLink href="#">2</PaginationLink>
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationEllipsis />
+					</PaginationItem>
+					<PaginationItem>
+						<PaginationNext href="#" />
+					</PaginationItem>
+				</PaginationContent>
+			</Pagination>
+
 			<ItemGrid items={items} isLoading={isLoading} />
 		</main>
 	);
