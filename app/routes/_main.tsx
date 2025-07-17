dfc4e30dcaffa295b79b9ae579a7ee4574dc5e3c
HoweZae <jdtomanan@up.edu.ph>
2025-07-17 20:52:15 +0800

    feat: make working together up until sort, order

--- app/routes/_main.tsx
+++ app/routes/_main.tsx
@@ -14,6 +14,7 @@ import {
 	PaginationNext,
 	PaginationPrevious,
 } from "~/components/ui/pagination"
+import { useSearch } from "~/lib/context/SearchContext";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -25,12 +26,21 @@ export function meta({}: Route.MetaArgs) {
 
 
 export default function Home() {
+	const { search, database, sort, order } = useSearch();
+
 	const [page, setPage] = useState(1);
 	const [characters, setCharacters] = useState<Character[]>([]);
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 
+	const url = `https://api.potterdb.com/v1/characters` + 
+			`?page[size]=20` +
+			`&page[number]=${page}` +
+			`&sort=${(order == "descending") ? '-' : ''}${sort}` +
+			`&filter[name_cont]=${search}`;
+
 	useEffect(() => {
-		axios.get(`https://api.potterdb.com/v1/characters?page[size]=20&page[number]=${page}`)
+		setIsLoading(true);
+		axios.get(url)
 				.then(res => {
 						const actualCharacters = res.data.data.map((item: { attributes: any; }) => item.attributes);
 						setCharacters(actualCharacters);
@@ -41,59 +51,67 @@ export default function Home() {
 				.finally(() => {
 						setIsLoading(false);
 				});
-	}, [page]);
+	}, [page, search, database, sort, order]);
 
   return (
 		<main className="w-screen px-1 md:px-2">
 			<Pagination className="bg-transparent border-none w-fit">
 				<PaginationContent>
-					<PaginationItem>
-						<PaginationPrevious onClick={(e) => {
-							e.preventDefault();
-							setIsLoading(true);
-							setPage(page-1);
-						}}/>
-					</PaginationItem>
+					{(page != 1) ? (
+						<PaginationItem>
+							<PaginationPrevious
+								size="icon"
+								onClick={(e) => {
+									e.preventDefault();
+									setPage(page-1);
+								}}
+							/>
+						</PaginationItem>
+					) : <></>}
+
 					{(page > 2) ? (
 						<PaginationItem>
 							<PaginationEllipsis />
 						</PaginationItem>
 					) : <></>}
+
 					{(page > 1) ? (
 						<PaginationItem>
 							<PaginationLink
 								onClick={(e) => {
 								e.preventDefault();
-								setIsLoading(true);
 								setPage(page-1);
 								}}>
 								{page-1}
 							</PaginationLink>
 						</PaginationItem>
 					) : <></>}
+
 					<PaginationItem>
 						<PaginationLink isActive href="#">{page}</PaginationLink>
 					</PaginationItem>
+					
 					<PaginationItem>
 						<PaginationLink onClick={(e) => {
 							e.preventDefault();
-							setIsLoading(true);
 							setPage(page+1);
 						}}>{page+1}</PaginationLink>
 					</PaginationItem>
+					
 					<PaginationItem>
 						<PaginationEllipsis />
 					</PaginationItem>
+					
 					<PaginationItem>
-						<PaginationNext onClick={(e) => {
+						<PaginationNext
+							size="icon"
+							onClick={(e) => {
 							e.preventDefault();
-							setIsLoading(true);
 							setPage(page+1);
 						}} />
 					</PaginationItem>
 				</PaginationContent>
 			</Pagination>
-
 			<ItemGrid items={characters} isLoading={isLoading} />
 		</main>
 	);
