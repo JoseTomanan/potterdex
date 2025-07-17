4222e9da2a037c45ea105fc59570f979a70ea86b
HoweZae <jdtomanan@up.edu.ph>
2025-07-17 17:45:18 +0800

    feat: add slightly better formatting

--- app/components/character/ItemDetails.tsx
+++ app/components/character/ItemDetails.tsx
@@ -12,30 +12,38 @@ export function ItemDetails(props: ItemDetailsProps) {
 			<h6>
 				SLUG: <span className="font-mono">{props.slug}</span>
 			</h6>
-			<div className="flex flex-col">
-				<h1>{props.name}</h1>
-				<span className="flex flex-row">
-					{props.born}
-					{props.died ? props.died : <></>}
-					{props.marital_status}
-				</span>
-				<h6>{props.gender}</h6>
-				<h6>{props.nationality}</h6>
-				<h6>{props.skin_color}</h6>
-				{props.species}
-				{props.wand}
-				{props.weight ? props.weight : <></>}
-				{props.house}
-				{props.alias_names}
-				{props.blood_status}
-			</div>
-			<div>
-				{props.image ? props.image : <></>}
-				{props.wiki ? (
-					<a href="props.wiki" target="_blank" className="hoverable-link">
-						Visit wiki
-					</a>
-				) : <></>}
+			<h1>{props.name}</h1>
+			<div className="flex flex-row">
+				<div className="flex flex-col">
+					<span className="flex flex-row">
+						<h6>Born: {props.born}</h6>
+						{props.died ? (
+								<h6>Died: {props.died}</h6>
+						) : <></>}
+					</span>
+					<h6>{props.marital_status}</h6>
+					<h6>{props.gender}</h6>
+					<h6>{props.nationality}</h6>
+					<h6>{props.skin_color}</h6>
+					<h6>{props.species}</h6>
+					<h6>{props.wand}</h6>
+					{props.weight ? (
+							<h6>props.weight</h6>
+					) : <></>}
+					<h6>{props.house}</h6>
+					<h6>{props.alias_names}</h6>
+					<h6>{props.blood_status}</h6>
+				</div>
+				<div>
+					{props.image ? (
+							<img src={props.image} />
+					) : <></>}
+					{props.wiki ? (
+							<a href="props.wiki" target="_blank" className="hoverable-link">
+								Visit wiki
+							</a>
+					) : <></>}
+				</div>
 			</div>
 		</section>
 	);
