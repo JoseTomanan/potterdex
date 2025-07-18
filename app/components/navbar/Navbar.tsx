4b5f89dd96aee4feaaddd4ae7e32e4be20b8f7fd
HoweZae <jdtomanan@up.edu.ph>
2025-07-18 09:10:06 +0800

    feat: fix case

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import {
+	Link,
+	useLocation,
+} from "react-router";
+
+import { Logo } from "./Logo";
+import { SearchBar } from "./SearchBar";
+
+export function NavBar() {
+	const isNotFixed = (useLocation().pathname === "/about");
+
+	return (
+		<nav className={`flex flex-row items-center justify-center w-full ${isNotFixed ? "relative" : "fixed"}`}>
+			<div className="flex items-center gap-6">
+				<Logo />
+				<SearchBar disabled={isNotFixed}/>
+			</div>
+			<span className="flex-1"/>
+			<div className="flex gap-8 justify-end">
+				<a href="https://potterdb.com/" target="_blank">PotterDB</a>
+				<Link to="/about">About</Link>
+			</div>
+		</nav>
+	);
+}
