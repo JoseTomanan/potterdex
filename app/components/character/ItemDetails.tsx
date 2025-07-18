c4c1022426f1af4dda271a6f3ede4376ba0f4663
HoweZae <jdtomanan@up.edu.ph>
2025-07-18 08:26:13 +0800

    feat: make better

--- app/components/character/ItemDetails.tsx
+++ app/components/character/ItemDetails.tsx
@@ -1,3 +1,4 @@
+import { IoPersonSharp } from "react-icons/io5";
 import type { Character } from "~/lib/types/character";
 
 
@@ -5,43 +6,59 @@ type ItemDetailsProps = Character;
 
 export function ItemDetails(props: ItemDetailsProps) {
 	return (
-		<section>
-			<h6 className="font-mono text-muted-foreground">
-				(PROPER FORMATTING TO FOLLOW)
-			</h6>
-			<h6>
-				SLUG: <span className="font-mono">{props.slug}</span>
-			</h6>
+		<section className="rounded bg-card border border-border px-2 py-4">
+			<p className="font-mono text-muted">
+				<b>slugname</b>: {props.slug}
+			</p>
 			<h1>{props.name}</h1>
-			<div className="flex flex-row">
-				<div className="flex flex-col">
-					<span className="flex flex-row">
-						<h6>Born: {props.born}</h6>
-						{props.died ? (
-								<h6>Died: {props.died}</h6>
+
+			<span className="text-lg text-muted-foreground">
+				<span>{props.house} &middot; {props.gender} &middot; {props.species} &middot; </span>
+				{props.wiki ? (
+					<a href="props.wiki" target="_blank" className="highlight-link">
+						Visit wiki
+					</a>
+				) : <></>}
+			</span>
+
+			<div className="flex flex-row gap-x-4">
+				<div className="flex flex-col gap-1 flex-1/3 px-2 py-1 bg-popover border">
+					<div className="flex justify-center align-middle bg-popover">
+						{props.image ? (
+							<img
+								src={props.image}
+								className="h-fit object-contain" />
+						) : (
+							<IoPersonSharp className="fill-muted size-full" />
+						)}
+					</div>
+					<hr></hr>
+					{props.born ? (
+							<span className="flex space-x-1 items-baseline">
+								<h5>Born: </h5>
+								<h6>{props.born}</h6>
+							</span>
+					) : <></>}
+					{props.died ? (
+							<span className="flex space-x-1 items-baseline">
+								<h5>Died: </h5>
+								<h6>{props.died}</h6>
+							</span>
 						) : <></>}
+					<span className="flex space-x-1 items-baseline">
+						<h5>Aliases: </h5>
+						<h6>{props.alias_names.map((i) => (<>{i}<br/></>))}</h6>
 					</span>
-					<h6>{props.marital_status}</h6>
-					<h6>{props.gender}</h6>
-					<h6>{props.nationality}</h6>
-					<h6>{props.skin_color}</h6>
-					<h6>{props.species}</h6>
-					<h6>{props.wand}</h6>
-					{props.weight ? (
-							<h6>props.weight</h6>
-					) : <></>}
-					<h6>{props.house}</h6>
-					<h6>{props.alias_names}</h6>
-					<h6>{props.blood_status}</h6>
 				</div>
-				<div>
-					{props.image ? (
-							<img src={props.image} />
-					) : <></>}
-					{props.wiki ? (
-							<a href="props.wiki" target="_blank" className="hoverable-link">
-								Visit wiki
-							</a>
+
+				<div className="flex flex-col flex-2/3">
+					<p>Blood status: {props.blood_status}</p>
+					<p>Marital status: {props.marital_status}</p>
+					<p>Nationality: {props.nationality}</p>
+					<p>Skin color: {props.skin_color}</p>
+					<p>Wand: {props.wand}</p>
+					{props.weight ? (
+							<p>Weight: {props.weight}</p>
 					) : <></>}
 				</div>
 			</div>
