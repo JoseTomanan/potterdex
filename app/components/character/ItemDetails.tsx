ccfb807fec4312e29bfe05f5673b400998ca8245
HoweZae <jdtomanan@up.edu.ph>
2025-07-18 08:35:51 +0800

    feat: segregate bio into own variable

--- app/components/character/ItemDetails.tsx
+++ app/components/character/ItemDetails.tsx
@@ -5,6 +5,41 @@ import type { Character } from "~/lib/types/character";
 type ItemDetailsProps = Character;
 
 export function ItemDetails(props: ItemDetailsProps) {
+	const Bio = (
+		<>
+			<div className="flex justify-center align-middle bg-popover">
+				{props.image ? (
+					<img
+						src={props.image}
+						className="h-fit object-contain" />
+				) : (
+					<IoPersonSharp className="fill-muted size-full" />
+				)}
+			</div>
+			<hr />
+			{props.born ? (
+					<span className="flex space-x-1 items-baseline">
+						<h5>Born: </h5>
+						<h6>{props.born}</h6>
+					</span>
+			) : <></>}
+			{props.died ? (
+					<span className="flex space-x-1 items-baseline">
+						<h5>Died: </h5>
+						<h6>{props.died}</h6>
+					</span>
+				) : <></>}
+			<span className="flex space-x-1 items-baseline">
+				<h5>Aliases: </h5>
+				<h6>
+					{props.alias_names.map((i) => (<>
+						{i} <br/>
+					</> ))}
+				</h6>
+			</span>
+		</>
+	);
+
 	return (
 		<section className="rounded bg-card border border-border px-2 py-4">
 			<p className="font-mono text-muted">
@@ -23,32 +58,7 @@ export function ItemDetails(props: ItemDetailsProps) {
 
 			<div className="flex flex-row gap-x-4">
 				<div className="flex flex-col gap-1 flex-1/3 px-2 py-1 bg-popover border">
-					<div className="flex justify-center align-middle bg-popover">
-						{props.image ? (
-							<img
-								src={props.image}
-								className="h-fit object-contain" />
-						) : (
-							<IoPersonSharp className="fill-muted size-full" />
-						)}
-					</div>
-					<hr></hr>
-					{props.born ? (
-							<span className="flex space-x-1 items-baseline">
-								<h5>Born: </h5>
-								<h6>{props.born}</h6>
-							</span>
-					) : <></>}
-					{props.died ? (
-							<span className="flex space-x-1 items-baseline">
-								<h5>Died: </h5>
-								<h6>{props.died}</h6>
-							</span>
-						) : <></>}
-					<span className="flex space-x-1 items-baseline">
-						<h5>Aliases: </h5>
-						<h6>{props.alias_names.map((i) => (<>{i}<br/></>))}</h6>
-					</span>
+					{Bio}
 				</div>
 
 				<div className="flex flex-col flex-2/3">
