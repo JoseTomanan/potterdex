b7cab947aa51f80bf1a7527661dadeec0a2f14a5
JoseTomanan big PC <joseernestotomanan@gmail.com>
2025-07-22 23:22:03 +0800

    refactor: make more readable

--- app/components/character/ItemGrid.tsx
+++ app/components/character/ItemGrid.tsx
@@ -9,23 +9,25 @@ type ItemGridProps = {
 }
 
 export function ItemGrid( props: ItemGridProps ) {
+	if (props.isLoading) {
+		return (
+			<div className="card-grid">
+				{Array.from({ length: ITEMS_PER_PAGE }).map((_, i) => (
+						<ItemSkeleton key={i} />
+				))}
+			</div>
+		);
+	}
+
 	return (
 		<div className="card-grid">
-			{props.isLoading
+			{props.items.length > 0
 				?
-					Array.from({ length: ITEMS_PER_PAGE }).map((_, i) => (
-						<ItemSkeleton key={i} />
+					props.items.map(({}, i) => (
+						<ItemCard {...props.items[i]} />
 					))
 				:
-					props.items.length > 0
-						?
-							props.items.map(({}, i) => (
-								<ItemCard {...props.items[i]} />
-							))
-						:
-							<>
-								<h6>No more results found.</h6>
-							</>
+					<h6>No more results found.</h6>
 			}
 		</div>
 	);
