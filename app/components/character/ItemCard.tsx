d348a13c709e5ecd1ac3493c3e8ac5c7d7cbe3b6
JoseTomanan <jdtomanan@up.edu.ph>
2025-07-26 10:32:56 +0800

    refactor: brevity

--- app/components/character/ItemCard.tsx
+++ app/components/character/ItemCard.tsx
@@ -12,8 +12,6 @@ import { Dialog, DialogTrigger } from "~/components/ui/dialog";
 import ItemModal from "./ItemModal";
 
 
-type ItemCardProps = Character;
-
 const getGenderIcon = (gender: String) => (
 			gender == "Male"
 			? <IoMale />
@@ -25,19 +23,18 @@ const getGenderIcon = (gender: String) => (
 		);
 
 
-export function ItemCard( item: ItemCardProps ) {
-	const GenderIcon = () => getGenderIcon(item.gender);
+export function ItemCard( props: Character ) {
+	const GenderIcon = () => getGenderIcon(props.gender);
 
-	const houseRelatedStyle = getHouseRelatedStyle(item.house);
-	
-	const nonHouseSubtitle = joinWithMiddot([item.nationality, item.species, item.blood_status]);
+	const houseRelatedStyle = getHouseRelatedStyle(props.house);	
+	const nonHouseSubtitle = joinWithMiddot([props.nationality, props.species, props.blood_status]);
 
 
 	return (
 		<div className="card gap-y-2 group hover:bg-muted">
 			<span className="h-70 flex justify-start align-top">
-				{item.image ? (
-					<img src={item.image} className="size-full bg-popover object-cover rounded shadow-inner"/>
+				{props.image ? (
+					<img src={props.image} className="size-full bg-popover object-cover rounded shadow-inner"/>
 				) : (
 					<IoPersonSharp className="size-full fill-popover rounded" />
 				)}
@@ -45,18 +42,18 @@ export function ItemCard( item: ItemCardProps ) {
 			<div className="px-3 pb-3">
 				<Dialog>
 					<DialogTrigger asChild>
-						<button className="flex items-center gap-1.5 w-full">
+						<button className="flex propss-center gap-1.5 w-full">
 							<GenderIcon />
 							<h3 className="text-left text-foreground/80 underline-offset-2 decoration-1 truncate hover:underline w-full cursor-pointer">
-								{item.name}
+								{props.name}
 							</h3>
 						</button>
 					</DialogTrigger>
-					<ItemModal {...item}/>
+					<ItemModal {...props}/>
 				</Dialog>
-				<h5 className="items-baseline" >
+				<h5 className="propss-baseline" >
 					<span className={`text-shadow-xs/20 ${houseRelatedStyle}`}>
-						{item.house ? item.house : "No house"}
+						{props.house ? props.house : "No house"}
 					</span>
 					{nonHouseSubtitle && (
 						<span className="font-light tracking-tight text-muted-foreground">
